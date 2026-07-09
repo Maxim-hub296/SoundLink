49acaf36063309dc3e4698b4708028ae074bd8d9
Arthax <itoppro11@gmail.com>
2026-07-10 00:39:06 +0300

    fix: обновить URL-маршруты для новых представлений

--- soundlink/music/urls.py
+++ soundlink/music/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
-from .views import *
+from django.urls import path
 
+from .views import SongUploadView, SongListView
 
 urlpatterns = [
-    path("test/", TestView.as_view(), name="test")
+    path("songs/upload/", SongUploadView.as_view(), name="song-upload"),
+    path("songs/", SongListView.as_view(), name="song-list"),
 ]
