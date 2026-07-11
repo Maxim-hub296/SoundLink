0f83eb334a8699e66adbb3c89c6b76fa491ffbbd
Arthax <itoppro11@gmail.com>
2026-07-11 14:21:59 +0300

    chore: переместил модель плейлистов в отдельное приложение под плейлисты

--- soundlink/music/models.py
+++ soundlink/music/models.py
@@ -18,21 +18,3 @@ class Song(models.Model):
 
     class Meta:
         ordering = ['-created_at']
-
-
-class Playlist(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    cover = models.ImageField(upload_to='playlists/', blank=True, null=True)
-    is_private = models.BooleanField(default=True)
-    is_for_friends = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ['-created_at']
