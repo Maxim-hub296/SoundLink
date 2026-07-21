31d962900dde2641e4d1e2da30d6792128b3bb81
Arthur <itoppro11@gmail.com>
2026-07-22 00:25:30 +0300

    feat: add playlist detail route and pk to delete

--- soundlink/playlists/urls.py
+++ soundlink/playlists/urls.py
@@ -5,6 +5,7 @@ from .views import PlaylistCreateView, PlaylistsView, PlaylistDeleteView
 app_name = "playlists"
 urlpatterns = [
     path("create/", PlaylistCreateView.as_view(), name="playlist-create"),
-    path("delete/", PlaylistDeleteView.as_view(), name="playlist-delete"),
+    path("delete/<int:pk>/", PlaylistDeleteView.as_view(), name="playlist-delete"),
     path("", PlaylistsView.as_view(), name="playlists-list"),
+    path("<int:pk>/", PlaylistsView.as_view(), name="playlists-detail"),
 ]
